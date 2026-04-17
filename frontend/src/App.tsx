4df70f0ead58b9e498dd8e01a279ecb2d757bfdd
Jeong Cheon uk <interstander@cgnmail.net>
2026-04-17 15:14:58 +0900

    Add /viewer redirect to /

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react'
 import ViewerPage from './pages/viewer/ViewerPage'
 import './styles/index.css'
 
 function App() {
+  useEffect(() => {
+    if (window.location.pathname === '/viewer') {
+      window.history.replaceState(null, '', '/')
+    }
+  }, [])
+
   return <ViewerPage />
 }
 
